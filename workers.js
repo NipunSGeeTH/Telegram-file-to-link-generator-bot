534805b1857177962941e5705d68403dff3310f9
Nipun Sangeeth <76957918+NipunSGeeTH@users.noreply.github.com>
2025-04-06 01:04:37 +0530

    Update workers.js

--- workers.js
+++ workers.js
@@ -5,7 +5,7 @@ const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
 const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text.
 const BOT_CHANNEL = ; // Insert your telegram channel id which the bot is admin in.
 const SIA_NUMBER = 1234; // Insert a random integer number and keep it safe.
-const BOT_OWNER = "NipunSGeeTH"; // Not numeric ID, use username for clickable button.
+const BOT_OWNER = ""; // Not numeric ID, use username for clickable button.
 // ----------Ohh Bhai Do Not Modify ---------- // 
 
 const WHITE_METHODS = ["GET", "POST", "HEAD"];
